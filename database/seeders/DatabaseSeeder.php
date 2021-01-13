96d5d5512013f211c772a062b5e0aa7919446c70
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2021-01-13 18:21:32 +0900

    InCategoriesTableSeeder.phpファイル実行処理の追加【DatabaseSeeder.php】

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         // $this->call(ExpendsTableSeeder::class);
         // $this->call(IncomesTableSeeder::class);
-        $this->call(ExCategoriesTableSeeder::class);
+        // $this->call(ExCategoriesTableSeeder::class);
+        $this->call(InCategoriesTableSeeder::class);
     }
 }

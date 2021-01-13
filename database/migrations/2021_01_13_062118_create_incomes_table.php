3a1c0089f622be2a4e3b81a4801c662d5dfe17e4
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2021-01-13 15:34:58 +0900

    migrationファイルの作成【create_incomes_table.php】

---
+++ database/migrations/2021_01_13_062118_create_incomes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateIncomesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incomes', function (Blueprint $table) {
+            $table->bigIncrements('income_id');
+            $table->string('income_name', 45);
+            $table->integer('income_price');
+            $table->date('income_date');
+            $table->integer('user_id');
+            $table->integer('in_category_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incomes');
+    }
+}

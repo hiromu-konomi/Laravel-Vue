9aa1e2ba562116cd8d8a2d2d8a14e69ab3eda50e
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2021-01-19 11:02:49 +0900

    cardを追加【Form.vue】

--- resources/js/components/Form.vue
+++ resources/js/components/Form.vue
@@ -1,5 +1,12 @@
 <template>
-    <h1>Form</h1>
+    <v-layout row wrap justify-center>
+        <v-flex xs5 mt-5>
+            <v-card>
+                <v-card-title>フォーム</v-card-title>
+                <v-card-text>jalfjjflajfdlsj;fa</v-card-text>
+            </v-card>
+        </v-flex>
+    </v-layout>
 </template>
 
 <script>

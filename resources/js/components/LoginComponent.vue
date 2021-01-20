ced59f3a398fae243b32acf4439d188997763a4b
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2021-01-20 11:56:42 +0900

    入力値チェック追加

--- resources/js/components/LoginComponent.vue
+++ resources/js/components/LoginComponent.vue
@@ -12,12 +12,14 @@
                                 label="メールアドレス"
                                 v-model="email"
                                 outlined
+                                :rules="[emailRules]"
                             ></v-text-field>
                             <v-text-field
                                 prepend-icon="mdi-lock"
                                 label="パスワード"
                                 v-model="password"
                                 outlined
+                                :rules="[passwordRules, passwordLimit]"
                             ></v-text-field>
                             <div>
                                 <v-btn color="teal lighten-3" @click="Login">ログイン</v-btn>
@@ -32,7 +34,15 @@
 
 <script>
 export default {
-
+    data() {
+        return {
+            emailRules: (value) => !!value || "メールアドレスを入力してください",
+            passwordRules: (value) => !!value || "パスワードを入力してください",
+            passwordLimit: (value) =>
+                /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{8,15}$/.test(value) ||
+                "8文字以上15文字以下で入力してください。ただし半角英小文字大文字数字を含んでください",
+        }
+    },
 }
 </script>
 

848c092141816af50e7c96d3a1cf1a15513ce262
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2021-01-26 12:36:08 +0900

    収入カテゴリーを全件取得する処理を追加【store/category.js】

--- resources/js/store/category.js
+++ resources/js/store/category.js
@@ -9,12 +9,22 @@ const mutations = {
     getExCateDatas(state, resData) {
         state.exCateDatas = resData;
     },
+
+    getInCateDatas(state, resData) {
+        state.inCateDatas = resData;
+    },
 }
 
 const actions = {
     async getExCateDatas({commit}) {
         await axios.get('/api/ex_categories').then((res) => {
             commit("getExCateDatas", res.data);
+        }).catch(error => console.log(error));
+    },
+
+    async getInCateDatas({commit}) {
+        await axios.get('/api/in_categories').then((res) => {
+            commit("getInCateDatas", res.data);
         }).catch(error => console.log(error));
     },
 }

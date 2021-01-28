75aa89ab9f458a7de68a9c754a8f6068340bb738
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2021-01-28 18:24:04 +0900

    ルーティングの追加【routes/api.php】

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\Api\RegisterController;
 use App\Http\Controllers\ExpendsController;
 use App\Http\Controllers\IncomesController;
 use App\Http\Controllers\ExCategoriesController;
@@ -21,19 +22,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/expends', [ExpendsController::class, 'index']);
 Route::post('/expends', [ExpendsController::class, 'store']);
 Route::delete('/expends/{expend}', [ExpendsController::class, 'destroy']);
+
 Route::get('/incomes', [IncomesController::class, 'index']);
 Route::post('/incomes', [IncomesController::class, 'store']);
 Route::delete('/incomes/{income}', [IncomesController::class, 'destroy']);
+
 Route::get('/ex_categories', [ExCategoriesController::class, 'index']);
 Route::post('/ex_categories', [ExCategoriesController::class, 'store']);
 Route::get('/ex_categories/{exCategory}', [ExCategoriesController::class, 'show']);
 Route::delete('/ex_categories/{exCategory}', [ExCategoriesController::class, 'destroy']);
+
 Route::get('/in_categories', [InCategoriesController::class, 'index']);
 Route::post('/in_categories', [InCategoriesController::class, 'store']);
 Route::get('/in_categories/{inCategory}', [InCategoriesController::class, 'show']);
 Route::delete('/in_categories/{inCategory}', [InCategoriesController::class, 'destroy']);
 
-
+Route::post('/register', [RegisterController::class, 'register']);
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
 // });

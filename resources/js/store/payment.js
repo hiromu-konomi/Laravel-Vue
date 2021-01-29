cff670fe3d12c63eb9f84186cc1eb355f23859ba
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2021-01-29 17:32:05 +0900

    apexchartsに必要なgetters処理追加

--- resources/js/store/payment.js
+++ resources/js/store/payment.js
@@ -3,6 +3,7 @@ import moment from "moment"
 const state = {
     exDatas: [],
     inDatas: [],
+    exCateDatas: [],
 }
 
 const getters = {
@@ -49,7 +50,110 @@ const getters = {
             events.push(inEvent);
         }
         return events;
-    }
+    },
+
+    getExTotalPriceByCategory(state) {
+        const totalExPriceByCategory = [];
+        for(var exCate of state.exCateDatas) {
+            const totalExPrice = 0;
+            for(var ex of state.exDatas) {
+                if(ex.ex_category_id === exCate.id){
+                    totalExPrice += ex.expend_price;
+                }else{
+                    continue;
+                }
+            }
+            totalExPriceByCategory.push(totalExPrice);
+        }
+        return totalExPriceByCategory;
+    },
+
+    getChartOptions(state) {
+        const expendCategoryIdDatas = [];
+        const exCategoryName = [];
+        for(var ex of state.exDatas) {
+            expendCategoryIdDatas.push(ex.ex_category_id);
+        }
+        const categoryIdAfterSet = Array.from(new Set(expendCategoryIdDatas));
+
+        for(var cateId of categoryIdAfterSet) {
+            for(var exCate of state.exCateDatas) {
+
+                if(cateId === exCate.id) {
+                    exCategoryName.push(exCate.ex_category_name);
+                }else{
+                    continue;
+                }
+            }
+        }
+        const exBackgroundColor = [];
+        const exBorderColor = [];
+
+        for(var exCate of state.exCateDatas) {
+
+            if(exCate.ex_category_color === 'red'){
+                exBackgroundColor.push('rgba(255, 0, 0, 0.6)');
+                exBorderColor.push('rgba(255, 0, 0, 1)');
+
+            }else if(exCate.ex_category_color === 'pink'){
+                exBackgroundColor.push('rgba(255, 192, 203, 0.6)');
+                exBorderColor.push('rgba(255, 192, 203, 1)');
+
+            }else if(exCate.ex_category_color === 'purple'){
+                exBackgroundColor.push('rgba(128, 0, 128, 0.6)');
+                exBorderColor.push('rgba(128, 0, 128, 1)');
+
+            }else if(exCate.ex_category_color === 'deep-purple'){
+                exBackgroundColor.push('rgba(148, 0, 211, 0.6)');
+                exBorderColor.push('rgba(148, 0, 211, 1)');
+
+            }else if(exCate.ex_category_color === 'indigo'){
+                exBackgroundColor.push('rgba(65, 105, 225, 0.6)');
+                exBorderColor.push('rgba(65, 105, 225, 1)');
+
+            }else if(exCate.ex_category_color === 'blue'){
+                exBackgroundColor.push('rgba(0, 0, 225, 0.6)');
+                exBorderColor.push('rgba(0, 0, 225, 1)');
+
+            }else if(exCate.ex_category_color === 'cyan'){
+                exBackgroundColor.push('rgba(0, 255, 255, 0.6)');
+                exBorderColor.push('rgba(0, 255, 211, 1)');
+
+            }else if(exCate.ex_category_color === 'teal'){
+                exBackgroundColor.push('rgba(0, 128, 128, 0.6)');
+                exBorderColor.push('rgba(0, 128, 128, 1)');
+
+            }else if(exCate.ex_category_color === 'green'){
+                exBackgroundColor.push('rgba(0, 255, 0, 0.6)');
+                exBorderColor.push('rgba(0, 255, 0, 1)');
+
+            }else if(exCate.ex_category_color === 'amber'){
+                exBackgroundColor.push('rgba(255, 193, 7, 0.6)');
+                exBorderColor.push('rgba(255, 193, 7, 1)');
+
+            }else if(exCate.ex_category_color === 'orange'){
+                exBackgroundColor.push('rgba(255, 165, 0, 0.6)');
+                exBorderColor.push('rgba(255, 165, 0, 1)');
+
+            }else if(exCate.ex_category_color === 'brown'){
+                exBackgroundColor.push('rgba(139, 69, 19, 0.6)');
+                exBorderColor.push('rgba(139, 69, 19, 1)');
+            }
+        }
+
+        const options = {
+            labels: exCategoryName,
+            title: {
+                text: '支出割合',
+                align: '',
+            },
+            colors: exBackgroundColor,
+            stroke: {
+                colors: exBorderColor
+            }
+        }
+        return options;
+    },
 }
 
 const mutations = {
@@ -59,6 +163,10 @@ const mutations = {
 
     getInDatas(state, resData) {
         state.inDatas = resData;
+    },
+
+    getExCateDatas(state, resData) {
+        state.exCateDatas = resData;
     },
 
     setExDatasWithCateDatas(state, exDatas) {
@@ -81,6 +189,12 @@ const actions = {
         await axios.get('/api/incomes').then((res) => {
             commit("getInDatas", res.data);
         });
+    },
+
+    async getExCateDatas({commit}) {
+        await axios.get('/api/ex_categories').then((res) => {
+            commit("getExCateDatas", res.data);
+        }).catch(error => console.log(error));
     },
 
     setExDatasWithCateDatas({commit}, exDatas) {

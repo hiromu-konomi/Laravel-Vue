d315f594be93b1c7e25da454d976c588626045c2
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2021-02-05 15:43:10 +0900

    stateの変更 / logout処理の変更【store/auth.js】

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -1,18 +1,33 @@
 const state = {
-    user: {},
+    user: {
+        id: undefined,
+        name: undefined
+    },
+    token: window.localStorage.getItem("auth")
 }
 
-const getters = {}
+const getters = {
+    userName(state) {
+        const userName = state.user.name;
+        console.log("userName = " + userName);
+        return userName;
+    }
+}
 
 const mutations = {
     setUser(state, user) {
-        state.user = user;
+        state.user.id = user.id;
+        state.user.name = user.name;
     },
+
+    removeUser(state) {
+        state.user.id = undefined;
+        state.user.name = undefined;
+    }
 }
 
 const actions = {
     register({commit}, request) {
-        console.log("request = " + request);
         axios.post('api/register', request).then((result) => {
             commit("setUser", result.data.user);
             localStorage.setItem("auth", "true");
@@ -22,13 +37,9 @@ const actions = {
     },
 
     logout({commit}) {
-        axios.post('api/logout', null, {
-            headers: {
-                Authorization: `Bearer ${state.token}`,
-            }
-        }).then((result) => {
-            commit("setUser", null);
-            commit("setToken", null);
+        axios.post('api/logout').then(() => {
+            commit("removeUser");
+            localStorage.removeItem("auth")
         }).catch(error => {
             console.log(`Error! HTTP Status: ${error}`);
         })

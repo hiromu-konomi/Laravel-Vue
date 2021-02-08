707a7e19e19e87045e5182c4239fe64f192a094c
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2021-02-08 15:52:53 +0900

    メインコンポーネントの親コンポーネントを新規作成【Main.vue】

---
+++ resources/js/components/Main.vue
@@ -0,0 +1,59 @@
+<template>
+    <v-app>
+        <Header/>
+
+        <v-main :style="{ background: $vuetify.theme.themes.light.background }">
+            <v-container fluid fill-height>
+                <router-view/>
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+import Header from './parts/Header.vue';
+
+export default {
+    components: {
+        Header
+    },
+
+    async created() {
+        await this.refresh();
+    },
+
+    methods: {
+        async refresh() {
+            await this.$store.dispatch('payment/getExDatas');
+            await this.$store.dispatch('payment/getInDatas');
+            await this.$store.dispatch('payment/getExCateDatas');
+            await this.$store.dispatch('category/getExCateDatas');
+            await this.$store.dispatch('category/getInCateDatas');
+            let exDatas = [];
+            let inDatas = [];
+            for (var exData of this.$store.state.payment.exDatas) {
+                if (exData.ex_category_id) {
+                    this.$store.dispatch('category/setExCateId', exData.ex_category_id);
+                    exData.ex_category_name = this.$store.getters['category/getExCateName'];
+                    exData.ex_category_color = this.$store.getters['category/getExCateColor'];
+                    exDatas.push(exData);
+                } else {
+                    exDatas.push(exData);
+                }
+            }
+            for (var inData of this.$store.state.payment.inDatas) {
+                if (inData.in_category_id) {
+                    this.$store.dispatch('category/setInCateId', inData.in_category_id);
+                    inData.in_category_name = this.$store.getters['category/getInCateName'];
+                    inData.in_category_color = this.$store.getters['category/getInCateColor'];
+                    inDatas.push(inData);
+                } else {
+                    inDatas.push(inData);
+                }
+            }
+            this.$store.dispatch('payment/setExDatasWithCateDatas', exDatas);
+            this.$store.dispatch('payment/setInDatasWithCateDatas', inDatas);
+        },
+    },
+}
+</script>

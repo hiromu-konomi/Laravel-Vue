a2f51ca33e86fd9b7de5a7d5374dbcfe8726cfcc
tatsuki-Kasa <tatsuki.kasawaki@rakus-partners.co.jp>
2021-02-08 12:22:48 +0900

    DBから持ってきたユーザーデータをstateに登録する処理

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -42,17 +42,16 @@ const actions = {
     },
 
     login({commit}, request) {
-            axios.post('/api/login', request)
+            axios.post('api/login', request)
                 .then((result) => {
                     console.log(result);
-                    commit("setUser", result.data.user);
-                    commit("setMessage", result.data.message);
+                    commit("setUser", result.data.user[0]);
+                    // commit("setMessage", result.data.message);
                     localStorage.setItem("auth", "true");
-                    console.log("user = " + result.data.user + " + " + this.state.user);
-                    console.log("message = " + result.data.message + " + " + this.state.message);
+                    // console.log("message = " + result.data.message + " + " + this.state.message);
                 })
                 .catch(error => {
-                    this.errors = error.response.data.errors;
+                    console.log(`Error! HTTP Status: ${error}`);
                 });
     },
 

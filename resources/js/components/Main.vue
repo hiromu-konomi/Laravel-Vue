0be4b5dd56bc4da049ec1b2f1adab3e0d0b6fa61
konomihiromu <hiromu.konomi@rakus-partners.co.jp>
2021-02-10 16:23:25 +0900

    refreshメソッドの処理を微修正【Main.vue】

--- resources/js/components/Main.vue
+++ resources/js/components/Main.vue
@@ -24,35 +24,38 @@ export default {
 
     methods: {
         async refresh() {
-            await this.$store.dispatch('payment/getExDatas');
-            await this.$store.dispatch('payment/getInDatas');
-            await this.$store.dispatch('payment/getExCateDatas');
-            await this.$store.dispatch('category/getExCateDatas');
-            await this.$store.dispatch('category/getInCateDatas');
-            let exDatas = [];
-            let inDatas = [];
-            for (var exData of this.$store.state.payment.exDatas) {
-                if (exData.ex_category_id) {
-                    this.$store.dispatch('category/setExCateId', exData.ex_category_id);
-                    exData.ex_category_name = this.$store.getters['category/getExCateName'];
-                    exData.ex_category_color = this.$store.getters['category/getExCateColor'];
-                    exDatas.push(exData);
-                } else {
-                    exDatas.push(exData);
+            const userId = this.$store.state.auth.user.id
+            if (userId) {
+                await this.$store.dispatch('payment/getExDatas');
+                await this.$store.dispatch('payment/getInDatas');
+                await this.$store.dispatch('payment/getExCateDatas', userId);
+                await this.$store.dispatch('category/getExCateDatas', userId);
+                await this.$store.dispatch('category/getInCateDatas', userId);
+                let exDatas = [];
+                let inDatas = [];
+                for (var exData of this.$store.state.payment.exDatas) {
+                    if (exData.ex_category_id) {
+                        this.$store.dispatch('category/setExCateId', exData.ex_category_id);
+                        exData.ex_category_name = this.$store.getters['category/getExCateName'];
+                        exData.ex_category_color = this.$store.getters['category/getExCateColor'];
+                        exDatas.push(exData);
+                    } else {
+                        exDatas.push(exData);
+                    }
                 }
-            }
-            for (var inData of this.$store.state.payment.inDatas) {
-                if (inData.in_category_id) {
-                    this.$store.dispatch('category/setInCateId', inData.in_category_id);
-                    inData.in_category_name = this.$store.getters['category/getInCateName'];
-                    inData.in_category_color = this.$store.getters['category/getInCateColor'];
-                    inDatas.push(inData);
-                } else {
-                    inDatas.push(inData);
+                for (var inData of this.$store.state.payment.inDatas) {
+                    if (inData.in_category_id) {
+                        this.$store.dispatch('category/setInCateId', inData.in_category_id);
+                        inData.in_category_name = this.$store.getters['category/getInCateName'];
+                        inData.in_category_color = this.$store.getters['category/getInCateColor'];
+                        inDatas.push(inData);
+                    } else {
+                        inDatas.push(inData);
+                    }
                 }
+                this.$store.dispatch('payment/setExDatasWithCateDatas', exDatas);
+                this.$store.dispatch('payment/setInDatasWithCateDatas', inDatas);
             }
-            this.$store.dispatch('payment/setExDatasWithCateDatas', exDatas);
-            this.$store.dispatch('payment/setInDatasWithCateDatas', inDatas);
         },
     },
 }
